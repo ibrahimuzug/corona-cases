186aad7bcfdb3cbc6546a927b9b16b882dc86801
İbrahim Uzuğ <ibrahimuzug7@gmail.com>
2020-04-04 01:57:01 +0300

    Working version of past

--- src/main/java/com/corona/CoronaApplication.java
+++ src/main/java/com/corona/CoronaApplication.java
@@ -2,14 +2,12 @@ package com.corona;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication
-@EnableR2dbcRepositories
 public class CoronaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CoronaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CoronaApplication.class, args);
+    }
 
 }
--- src/main/java/com/corona/dto/SurviveDto.java
+++ src/main/java/com/corona/dto/SurviveDto.java
@@ -3,11 +3,13 @@ package com.corona.dto;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.Accessors;
 
 @RequiredArgsConstructor
 @Accessors(fluent = true)
 @Getter
+@Setter
 public class SurviveDto {
     @NonNull
     private final String country;
